6d60580bb8d624897ef9624255a38cbb1b495619
Jari Aalto <jari.aalto@cante.net>
2011-12-30 15:47:24 +0200

    epackage/: new
    Signed-off-by: Jari Aalto <jari.aalto@cante.net>

---
+++ epackage/smart-tab-epkg-install.el
@@ -0,0 +1,5 @@
+(autoload 'smart-tab-default                    "smart-tab" "" t)
+(autoload 'smart-tab                            "smart-tab" "" t)
+(autoload 'smart-tab-mode                       "smart-tab" "" t)
+(autoload 'global-smart-tab-mode                "smart-tab" "" t)
+(provide 'smart-tab-epkg-install)
